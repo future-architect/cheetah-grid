c0f6bbbbcdbd7485447d751673b5235e7bb1578e
yosuke ota <otameshiyo23@gmail.com>
2018-09-22 00:53:04 +0900

    Fixed #86

--- packages/vue-cheetah-grid/lib/c-grid/header-utils.js
+++ packages/vue-cheetah-grid/lib/c-grid/header-utils.js
@@ -5,32 +5,36 @@ const hyphenateRE = /\B([A-Z])/g
 const hyphenate = (str) => str.replace(hyphenateRE, '-$1').toLowerCase()
 
 function vnodeToColumn (vnode) {
+  if (!vnode.componentOptions ||
+    hyphenate(vnode.componentOptions.tag).indexOf('c-grid') !== 0) {
+    throw new Error(`unknown tag: ${(vnode.componentOptions && vnode.componentOptions.tag) || vnode.tag}`)
+  }
   if (vnode.componentInstance &&
-    vnode.componentOptions &&
-    hyphenate(vnode.componentOptions.tag).indexOf('c-grid') === 0 &&
     typeof vnode.componentInstance.createColumn === 'function') {
     return vnode.componentInstance.createColumn()
   }
-
-  throw new Error(`unknown tag: ${vnode.tag}`)
+  // before instantiation?
+  return undefined
 }
 function vnodeToColumnProp (vnode) {
+  if (!vnode.componentOptions ||
+    hyphenate(vnode.componentOptions.tag).indexOf('c-grid') !== 0) {
+    throw new Error(`unknown tag: ${(vnode.componentOptions && vnode.componentOptions.tag) || vnode.tag}`)
+  }
   if (vnode.componentInstance &&
-    vnode.componentOptions &&
-    hyphenate(vnode.componentOptions.tag).indexOf('c-grid') === 0 &&
     typeof vnode.componentInstance.createColumn === 'function') {
     return vnode.componentInstance.$_CGridColumn_getProps()
   }
-
-  throw new Error(`unknown tag: ${vnode.tag}`)
+  // before instantiation?
+  return undefined
 }
 function filterHeaderElements (slots) {
   return slots ? slots.filter(vnode => !!vnode.tag) : []
 }
 export function slotsToHeaderOptions (slots) {
-  return filterHeaderElements(slots).map(vnode => vnodeToColumn(vnode))
+  return filterHeaderElements(slots).map(vnode => vnodeToColumn(vnode)).filter(c => c)
 }
 
 export function slotsToHeaderProps (slots) {
-  return filterHeaderElements(slots).map(vnode => vnodeToColumnProp(vnode))
+  return filterHeaderElements(slots).map(vnode => vnodeToColumnProp(vnode)).filter(p => p)
 }
