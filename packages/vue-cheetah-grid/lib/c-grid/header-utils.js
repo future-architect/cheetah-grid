6d109160f61ccc5723250a52766fb053c9614005
ota <y.ohta.z3@future.co.jp>
2020-01-08 12:03:08 +0900

    Fixed a bug where vue component columns were not rendered.

--- packages/vue-cheetah-grid/lib/c-grid/header-utils.js
+++ packages/vue-cheetah-grid/lib/c-grid/header-utils.js
@@ -12,26 +12,35 @@ export function slotsToHeaderProps (cgridVm, slots) {
 }
 
 function getHeaderInstances (cgridVm, slots) {
-  return slots ? getColumnDefines(cgridVm, slots) : []
+  return slots ? getColumnDefinesFromVNodes(cgridVm, slots) : []
 }
 
-function getColumnDefines (cgridVm, vnodes) {
-  const results = []
-  vnodes.forEach(vnode => {
+function getColumnDefinesFromVNodes (cgridVm, vnodes) {
+  return getColumnDefinesFromInstances(vnodes.map(vnode => {
     if (!vnode.tag) {
-      return
+      return null
     }
     if (!vnode.componentInstance) {
+      return null
+    }
+    return vnode.componentInstance
+  }))
+}
+
+function getColumnDefinesFromInstances (cgridVm, instances) {
+  const results = []
+  instances.forEach(componentInstance => {
+    if (!componentInstance) {
       return
     }
-    if (typeof vnode.componentInstance.createColumn === 'function') {
+    if (typeof componentInstance.createColumn === 'function') {
       const defineColumns = cgridVm.$_CGrid_defineColumns
-      if (defineColumns.indexOf(vnode.componentInstance) >= 0) {
-        results.push(vnode.componentInstance)
+      if (defineColumns.indexOf(componentInstance) >= 0) {
+        results.push(componentInstance)
         return
       }
     }
-    results.push(...getColumnDefines(cgridVm, vnode.componentInstance.$children))
+    results.push(...getColumnDefinesFromInstances(cgridVm, componentInstance.$children))
   })
 
   return results
