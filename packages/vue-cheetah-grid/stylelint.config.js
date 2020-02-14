d675be85b6a4efb450a0e9d03e52f9006b297444
Yosuke Ota <otameshiyo23@gmail.com>
2020-02-14 23:42:20 +0900

    stylelint (#204)
    * stylelint
    
    * update
    
    * update

---
+++ packages/vue-cheetah-grid/stylelint.config.js
@@ -0,0 +1,10 @@
+'use strict'
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'font-family-no-missing-generic-family-keyword': null,
+    'rule-empty-line-before': 'never',
+    'number-leading-zero': 'never'
+    // 'declaration-colon-space-after': 'never',
+  }
+}
