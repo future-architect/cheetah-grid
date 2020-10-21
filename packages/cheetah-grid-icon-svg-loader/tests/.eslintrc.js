f2fdbc095f109744eb40fd1aa9ff2b05cc9814c8
ota <y.ohta.z3@future.co.jp>
2020-10-21 11:45:46 +0900

    update

--- packages/cheetah-grid-icon-svg-loader/tests/.eslintrc.js
+++ packages/cheetah-grid-icon-svg-loader/tests/.eslintrc.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
 module.exports = {
-  "extends": ["plugin:node/recommended"],
-  "rules": {
-      "node/exports-style": ["error", "module.exports"],
-      "node/prefer-global/buffer": ["error", "always"],
-      "node/prefer-global/console": ["error", "always"],
-      "node/prefer-global/process": ["error", "always"],
-      "node/prefer-global/url-search-params": ["error", "always"],
-      "node/prefer-global/url": ["error", "always"],
-      "no-console": "off",
-  }
-}
+	'extends': ['plugin:node/recommended'],
+	'rules': {
+		'node/exports-style': ['error', 'module.exports'],
+		'node/prefer-global/buffer': ['error', 'always'],
+		'node/prefer-global/console': ['error', 'always'],
+		'node/prefer-global/process': ['error', 'always'],
+		'node/prefer-global/url-search-params': ['error', 'always'],
+		'node/prefer-global/url': ['error', 'always'],
+		'no-console': 'off',
+	}
+};
